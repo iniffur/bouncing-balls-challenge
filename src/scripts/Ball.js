fd8f88ce762c96d7f774cab416127afd60321dcd
iniffur <jamesruffini97@gmail.com>
2023-03-09 22:21:47 +0000

    Implemented Ball class with Draw and Update functions

---
+++ src/scripts/Ball.js
@@ -0,0 +1,33 @@
+class Ball {
+  constructor(x, y, radius, dx, dy, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.dx = dx;
+    this.dy = dy;
+    this.color = color;
+  }
+
+  draw = (context) => {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fillStyle = this.color;
+    context.fill();
+    context.closePath();
+  };
+
+  update = (canvasWidth, canvasHeight) => {
+    if (this.x + this.radius > canvasWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > canvasHeight) {
+      this.dy = -this.dy * 0.8;
+    } else {
+      this.dy += 1;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+  };
+}
+
+module.exports = Ball;
