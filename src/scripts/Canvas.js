ebc6b3f505d4cfcdf888f3dbdf1282d344a04e11
iniffur <jamesruffini97@gmail.com>
2023-03-09 22:31:23 +0000

    Added functions to canvas class - addBall(), clear(), drawBalls() and updateBalls()

--- src/scripts/Canvas.js
+++ src/scripts/Canvas.js
@@ -6,4 +6,26 @@ class Canvas {
     this.height = canvas.height;
     this.balls = [];
   }
+
+  addBall = (ball) => {
+    this.balls.push(ball);
+  };
+
+  clear = () => {
+    this.context.clearRect(0, 0, this.width, this.height);
+  };
+
+  drawBalls = () => {
+    for (const ball of this.balls) {
+      ball.draw(this.context);
+    }
+  };
+
+  updateBalls = () => {
+    for (const ball of this.balls) {
+      ball.update(this.width, this.height);
+    }
+  };
 }
+
+module.exports = Canvas;
